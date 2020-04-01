6e32b72610b15458a68e621e2a9e112d0bf264b4
Jaime Hisao <jaimhisao2011@gmail.com>
2020-04-01 11:24:31 -0600

    modified: Changed stuff in game - NonCompiling

--- src/spaceinvaders/Game.java
+++ src/spaceinvaders/Game.java
@@ -84,9 +84,158 @@ public class Game implements Runnable {
     }
     
     private void tick(){
-    
+        
+        if (deaths == Commons.NUMBER_OF_ALIENS_TO_DESTROY) {
+
+            running = false;
+            timer.stop();
+            message = "Game won!";
+        }
+
+        //Tick the Player
+        player.tick();
+
+        // shot
+        if (shot.isVisible()) {
+
+            int shotX = shot.getX();
+            int shotY = shot.getY();
+
+            for (Alien alien : aliens) {
+
+                int alienX = alien.getX();
+                int alienY = alien.getY();
+
+                if (alien.isVisible() && shot.isVisible()) {
+                    if (shotX >= (alienX)
+                            && shotX <= (alienX + Commons.ALIEN_WIDTH)
+                            && shotY >= (alienY)
+                            && shotY <= (alienY + Commons.ALIEN_HEIGHT)) {
+
+                        alien.setImage(alien.loadImage("/resources/explosion.png"));
+                        alien.setDying(true);
+                        deaths++;
+                        shot.die();
+                    }
+                }
+            }
+
+            int y = shot.getY();
+            y -= 4;
+
+            if (y < 0) {
+                shot.die();
+            } else {
+                shot.setY(y);
+            }
+        }
+
+        //Tick the Aliens
+        for(Alien alien: aliens){
+            alien.tick();
+        }
+        
+        
+        for (Alien alien : aliens) {
+
+            int x = alien.getX();
+
+            if (x >= Commons.BOARD_WIDTH - Commons.BORDER_RIGHT && direction != -1) {
+
+                direction = -1;
+
+                Iterator<Alien> i1 = aliens.iterator();
+
+                while (i1.hasNext()) {
+
+                    Alien a2 = i1.next();
+                    a2.setY(a2.getY() + Commons.GO_DOWN);
+                }
+            }
+
+            if (x <= Commons.BORDER_LEFT && direction != 1) {
+
+                direction = 1;
+
+                Iterator<Alien> i2 = aliens.iterator();
+
+                while (i2.hasNext()) {
+
+                    Alien a = i2.next();
+                    a.setY(a.getY() + Commons.GO_DOWN);
+                }
+            }
+        }
+
+        Iterator<Alien> it = aliens.iterator();
+
+        while (it.hasNext()) {
+
+            Alien alien = it.next();
+
+            if (alien.isVisible()) {
+
+                int y = alien.getY();
+
+                if (y > Commons.GROUND - Commons.ALIEN_HEIGHT) {
+                    inGame = false;
+                    message = "Invasion!";
+                }
+
+                alien.act(direction);
+            }
+        }
+
+        // bombs
+        Random generator = new Random();
+
+        for (Alien alien : aliens) {
+
+            int shot = generator.nextInt(15);
+            Alien.Bomb bomb = alien.getBomb();
+
+            if (shot == Commons.CHANCE && alien.isVisible() && bomb.isDestroyed()) {
+
+                bomb.setDestroyed(false);
+                bomb.setX(alien.getX());
+                bomb.setY(alien.getY());
+            }
+
+            int bombX = bomb.getX();
+            int bombY = bomb.getY();
+            int playerX = player.getX();
+            int playerY = player.getY();
+
+            if (player.isVisible() && !bomb.isDestroyed()) {
+
+                if (bombX >= (playerX)
+                        && bombX <= (playerX + Commons.PLAYER_WIDTH)
+                        && bombY >= (playerY)
+                        && bombY <= (playerY + Commons.PLAYER_HEIGHT)) {
+
+                    player.setImage(player.loadImage("/resources/explosion.png"));
+                    player.setDying(true);
+                    bomb.setDestroyed(true);
+                }
+            }
+
+            if (!bomb.isDestroyed()) {
+
+                bomb.setY(bomb.getY() + 1);
+
+                if (bomb.getY() >= Commons.GROUND - Commons.BOMB_HEIGHT) {
+
+                    bomb.setDestroyed(true);
+                }
+            }
+        }
     }
     
+    
+    /**
+     * Renders the Game and its components
+     * @author Jaime Hisao
+     */
     private void render(){
          //Retrieves the BS from Display
         bs = display.getCanvas().getBufferStrategy();
@@ -100,7 +249,36 @@ public class Game implements Runnable {
         
         //Show Text for hits, lives and score
         g.setColor(Color.red);
-        g
+       
+        
+        //Render Individual Game Components
+        player.render(g);
+        
+        for(Alien alien:aliens){
+            alien.render(g);
+        }
+        
+        bs.show();
+        g.dispose();
+    }
+    
+    private void gameOver(Graphics g) {
+
+        g.setColor(Color.black);
+        g.fillRect(0, 0, Commons.BOARD_WIDTH, Commons.BOARD_HEIGHT);
+
+        g.setColor(new Color(0, 32, 48));
+        g.fillRect(50, Commons.BOARD_WIDTH / 2 - 30, Commons.BOARD_WIDTH - 100, 50);
+        g.setColor(Color.white);
+        g.drawRect(50, Commons.BOARD_WIDTH / 2 - 30, Commons.BOARD_WIDTH - 100, 50);
+
+        Font small = new Font("Helvetica", Font.BOLD, 14);
+        FontMetrics fontMetrics = this.getFontMetrics(small);
+
+        g.setColor(Color.white);
+        g.setFont(small);
+        g.drawString("Game Over", (Commons.BOARD_WIDTH - fontMetrics.stringWidth("Game Over")) / 2,
+                Commons.BOARD_WIDTH / 2);
     }
     
     /**
@@ -300,24 +478,7 @@ public class Game implements Runnable {
         Toolkit.getDefaultToolkit().sync();
     }
 
-    private void gameOver(Graphics g) {
-
-        g.setColor(Color.black);
-        g.fillRect(0, 0, Commons.BOARD_WIDTH, Commons.BOARD_HEIGHT);
-
-        g.setColor(new Color(0, 32, 48));
-        g.fillRect(50, Commons.BOARD_WIDTH / 2 - 30, Commons.BOARD_WIDTH - 100, 50);
-        g.setColor(Color.white);
-        g.drawRect(50, Commons.BOARD_WIDTH / 2 - 30, Commons.BOARD_WIDTH - 100, 50);
-
-        Font small = new Font("Helvetica", Font.BOLD, 14);
-        FontMetrics fontMetrics = this.getFontMetrics(small);
-
-        g.setColor(Color.white);
-        g.setFont(small);
-        g.drawString(message, (Commons.BOARD_WIDTH - fontMetrics.stringWidth(message)) / 2,
-                Commons.BOARD_WIDTH / 2);
-    }
+    
 
     private void update() {
 
@@ -478,6 +639,30 @@ public class Game implements Runnable {
         }
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     private class TAdapter extends KeyAdapter {
 
         @Override
