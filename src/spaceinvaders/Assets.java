b31bc2714e25dc8d2e39c8bb2aed6a1ee9a16ad2
RodrigoCasale <rodrigo_casale22@hotmail.com>
2020-04-01 12:21:46 -0600

    movimiento del personaje

--- src/spaceinvaders/Assets.java
+++ src/spaceinvaders/Assets.java
@@ -13,16 +13,7 @@ import java.awt.image.BufferedImage;
  * @author Rodrigo Casale
  */
 public class Assets {
-<<<<<<< HEAD
-<<<<<<< HEAD
     public static BufferedImage background;
-=======
-
->>>>>>> fa9a7c175c6ce065229ccac057b3056aedc98dbf
-=======
-    
-    public static BufferedImage background;
->>>>>>> 053de131dd44b9292e8b373ca92f3bc21b5fc5e8
     public static BufferedImage player;
     public static BufferedImage explosion;
     public static BufferedImage alien;
@@ -32,9 +23,6 @@ public class Assets {
         player = ImageLoader.loadImage("/resources/player.png");
         explosion = ImageLoader.loadImage("/resources/explosion.png");
         background = ImageLoader.loadImage("/resources/background.png");
-<<<<<<< HEAD
         alien = ImageLoader.loadImage("/resources/alien.png");
-=======
->>>>>>> 053de131dd44b9292e8b373ca92f3bc21b5fc5e8
     }
 }
